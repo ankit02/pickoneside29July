1f2c7833bac6ca6237ee9860186ad7ce638f591d
ssaaurabh <saurabh.singla@intuzion.com>
2013-03-05 14:30:12 +0530

    le le

---
+++ pickoneside/app/models/war.rb
@@ -0,0 +1,3 @@
+class War < ActiveRecord::Base
+  # attr_accessible :title, :body
+end
---
+++ pickoneside/db/migrate/20130305085819_create_wars.rb
@@ -0,0 +1,9 @@
+class CreateWars < ActiveRecord::Migration
+  def change
+    create_table :wars do |t|
+
+    	t.string :topic
+      t.timestamps
+    end
+  end
+end
