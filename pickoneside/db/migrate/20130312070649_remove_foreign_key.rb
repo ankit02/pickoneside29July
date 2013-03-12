655701c1e5aa2285dd07e3372be4e331ce7669ff
ssaaurabh <saurabh.singla@intuzion.com>
2013-03-12 17:05:21 +0530

    mergeduser

--- pickoneside/db/migrate/20130312070649_remove_foreign_key.rb
+++ pickoneside/db/migrate/20130312070649_remove_foreign_key.rb
@@ -1,37 +1,37 @@
 class RemoveForeignKey < ActiveRecord::Migration
   def up
 
-  	remove_foreign_key :wars, :categories
+  	#remove_foreign_key :wars, :categories
 
   	#add_column :options, :war_id, :Integer
 
-  	remove_foreign_key :options, :wars
+  	#remove_foreign_key :options, :wars
 
   	#add_column :comments, :war_id, :Integer
 
-  	remove_foreign_key :comments, :wars
+  	#remove_foreign_key :comments, :wars
 
   	#add_column :comments, :user_id, :Integer
 
-  	remove_foreign_key :comments, :users
+  	#remove_foreign_key :comments, :users
 
   	#add_column :votings, :option_id, :Integer
 
- 	remove_foreign_key :votings, :options
+ 	#remove_foreign_key :votings, :options
 
  	#add_column :votings, :user_id, :Integer
 
-  	remove_foreign_key :votings, :users
+  	#remove_foreign_key :votings, :users
 
   	#add_column :hits, :user_id, :Integer
 
-	remove_foreign_key :hits, :users  	
+	#remove_foreign_key :hits, :users  	
 
 	#add_column :hits, :war_id, :Integer
 
-	remove_foreign_key :hits, :wars
+	#remove_foreign_key :hits, :wars
 
-	remove_foreign_key :wars, :users
+	#remove_foreign_key :wars, :users
   end
 
   def down
