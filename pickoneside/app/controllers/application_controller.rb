8a7925ebaf90b8ecc926feab084f377d402d77a1
ssaaurabh <saurabh.singla@intuzion.com>
2013-03-22 15:28:11 +0530

    hottest wars

--- pickoneside/app/controllers/application_controller.rb
+++ pickoneside/app/controllers/application_controller.rb
@@ -3,11 +3,17 @@ class ApplicationController < ActionController::Base
 
   before_filter :load_sidebar
 
-def load_sidebar
-  @wars = War.all
-  
-end
+	def load_sidebar
+  		@wars = War.find(:all, :limit => 10,:order => 'created_at DESC')
 
+      @warsIds = Hit.find_by_sql ["SELECT war_id FROM hits group by 
+        war_id order by count(war_id) desc limit 10"]
 
+      @warsHot = []
+      @warsIds.each do |warId|
+        iWar = War.find(warId.war_id)
+        @warsHot.push(iWar)
+      end
 
+	end
 end
