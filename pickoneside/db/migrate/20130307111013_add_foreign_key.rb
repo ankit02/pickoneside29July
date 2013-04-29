6de68628cde64fc09659752f2dd14fb9fe81d63a
Mitali <mitsie@Nyu.(none)>
2013-04-29 12:27:57 +0530

    cssmodified

--- pickoneside/db/migrate/20130307111013_add_foreign_key.rb
+++ pickoneside/db/migrate/20130307111013_add_foreign_key.rb
@@ -1,36 +1,6 @@
 class AddForeignKey < ActiveRecord::Migration
   def up
-	add_column :wars, :category_id, :Integer
-
-  	add_foreign_key :wars, :categories
-
-  	add_column :options, :war_id, :Integer
-
-  	add_foreign_key :options, :wars
-
-  	add_column :comments, :war_id, :Integer
-
-  	add_foreign_key :comments, :wars
-
-  	add_column :comments, :user_id, :Integer
-
-  	add_foreign_key :comments, :users
-
-  	add_column :votings, :option_id, :Integer
-
- 	add_foreign_key :votings, :options
-
- 	add_column :votings, :user_id, :Integer
-
-  	add_foreign_key :votings, :users
-
-  	add_column :hits, :user_id, :Integer
-
-	add_foreign_key :hits, :users  	
-
-	add_column :hits, :war_id, :Integer
-
-	add_foreign_key :hits, :wars
+	
   	
   end
 
